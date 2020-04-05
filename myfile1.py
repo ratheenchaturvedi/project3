b037aec09fa723ae6ff5ec9d681774520a0d513e
ratheen <ratheenchaturvedi@gmail.com>
2020-04-05 07:18:04 +0530

    commiting the files

---
+++ myfile1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 20:41:22 2020
+
+@author: ratheen_chaturvedi
+"""
+
+def f1(x=10,y=3):
+    result1 = x*y
+    return(result1)
---
+++ myfile2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 20:43:03 2020
+
+@author: ratheen_chaturvedi
+"""
+
+def f1(x=10,y=2):
+    k1 = x*y
+    return(k1)
